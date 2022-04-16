60f0ebc29268bea27561f3547908c9c5e6e6fcce
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-04-16 03:11:06 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import pageData from './pageData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header pageData={pageData}/>
+        <About pageData={pageData}/>
+        <Resume pageData={pageData}/>
+        <Portfolio pageData={pageData}/>
+        <Testimonials pageData={pageData}/>
+        <ContactUs pageData={pageData}/>
+        <Footer pageData={pageData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
